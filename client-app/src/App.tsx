110cd3b6ec97207e94a34a39b49494d6b36bb4fa
3xP3rT <3xP3rT@DESKTOP-FSHNI2D>
2021-04-05 03:19:17 +0600

    cls 28

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ducks } from './demo';
+import axios from 'axios';
 
 function App() {
+  const [activities,setactivities]= useState([])
+  useEffect(()=>{
+    axios.get('http://localhost:5000/Activites').then( response =>{
+      console.log(response)
+setactivities(response.data)
+    })
+  },[])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p style={{color:'Green'}}>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+       <ul>
+         {activities.map((activity:any) => (
+           <li key={activity.id}>
+             {activity.title}
+           </li>
+         ))}
+       </ul>
       </header>
     </div>
   );
